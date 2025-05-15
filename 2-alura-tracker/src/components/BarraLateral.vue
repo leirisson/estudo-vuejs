a27b4382969e918ece57387613614f7b396f5824
leirisson <leirissonsouza99@gmail.com>
2025-05-15 17:00:17 -0400

    começando projeto

---
+++ 2-alura-tracker/src/components/BarraLateral.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="logo da alura track">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+/**todo o script typescript */
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+/**todo o estilo do componente */
+header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+}
+
+header > h1 > img {
+    border-radius: 10px;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
---
+++ 2-alura-tracker/src/components/Formulario.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formuláriopara a criaçãode uma nova tarefa">
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+</script>
+
+
+<style scoped>
+</style>
