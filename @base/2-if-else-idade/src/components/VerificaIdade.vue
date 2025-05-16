8ab9815d88a4abbed1d6e0969582f70a9500cd29
leirisson <103783610+leirisson@users.noreply.github.com>
2025-05-16 18:39:39 -0400

    verificando idade e criando login app

---
+++ @base/2-if-else-idade/src/components/VerificaIdade.vue
@@ -0,0 +1,20 @@
+<template>
+    <p v-if="idade2 > 10">Você é uma criança.</p>
+    <p v-else-if="idade1 < 18 ">Você é um adolescente.</p>
+    <p v-else>Você é um adulto.</p>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: "VerificaIdade",
+    data() {
+
+        return {
+            idade1: 78,
+            idade2: 5,
+            idade3: 16
+        }
+
+    }
+})
+</script>
