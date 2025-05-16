7366a295394e8f122a0b95ae1d4840a874c74cb7
leirisson <leirissonsouza99@gmail.com>
2025-05-16 16:55:07 -0400

    primeiro if

---
+++ @base/1-diretivas-de-decisao/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <p v-if="isLoggedIn">Bem-vindo, usuário!</p>
+    <button @click="logar">Logar</button>
+    <button @click="sair">Sair</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "IsLogged",
+    data(){
+        return {
+            isLoggedIn: false
+        }
+    },
+    methods: {
+        logar(){
+            this.isLoggedIn = true
+        },
+        sair(){
+            this.isLoggedIn = false
+        }
+    }
+})
+
+</script>
+
+<style scoped></style>
